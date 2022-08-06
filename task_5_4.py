be97bdc39cfc2ed201a486548ef4dc8144f13e37
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-08-06 18:47:34 +0200

    get movie, get series

--- task_5_4.py
+++ task_5_4.py
@@ -1,5 +1,3 @@
-from faker import Faker
-fake = Faker()
 import random
 
 
@@ -14,7 +12,7 @@ class Movie:
         self.number_of_plays += 1
 
     def __str__(self):
-        return f'{self.title} {self.publishment}'
+        return f'{self.title} {(self.publishment)}'
 
     def __repr__(self):
         return f'Movie(title={self.title}, publishment={self.publishment}, grade={self.grade}, number_of_plays={self.number_of_plays})'
@@ -38,16 +36,22 @@ class Series(Movie):
 
 library_list = []
 
-sort_library_list = sorted(library_list, key=lambda movie: movie.title)
-
-def get_movie():
-    pass
+def get_movies():
+    sort_by_title = sorted(library_list, key=lambda movie: movie.title)
+    for i in sort_by_title:
+        if not isinstance(i, Series) == True:
+            print(f'{i.title} ({i.publishment})')
 
 def get_series():
-    pass
+    sort_by_title = sorted(library_list, key=lambda series: series.title)
+    for i in sort_by_title:
+        if isinstance(i, Series) == True:
+            print(f'"{i.title} S{i.season:02d}E{i.episode:02d}"')
 
 def search(title):
-    pass
+    
+
+
 
 def generate_views():
     random_movie = random.choice(library_list)
@@ -70,12 +74,20 @@ if __name__ == '__main__':
     series2 = Series(title='The Crown', publishment=2016, grade='Drama', number_of_plays=20, episode=7, season=1)
     series3 = Series(title='The Queen s Gambit', publishment=2020, grade='Action', number_of_plays=50, episode=3, season=5)
 
-    movie.append_library_list()
-    movie2.append_library_list()
-    movie3.append_library_list()
-    series.append_library_list()
-    series2.append_library_list()
-    series3.append_library_list()
+    library_list.append(movie)
+    library_list.append(movie2)
+    library_list.append(movie3)
+    library_list.append(series)
+    library_list.append(series2)
+    library_list.append(series3)
 
-    generate_views()
-    generate_views_times()
+    choice = input('Podaj tytuł: ')
+    if ch
+
+    #print(library_list)
+    #generate_views()
+    #generate_views_times()
+    get_movies()
+    print()
+    get_series()
+    search()

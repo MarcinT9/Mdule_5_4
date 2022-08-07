1518a292b0c432465e07d0284a4a020854559dc8
Marcin Topczewski <marcintopczewski90@gmail.com>
2022-08-07 19:27:00 +0200

    fourth commit

--- task_5_4.py
+++ task_5_4.py
@@ -7,7 +7,7 @@ class Movie:
         self.title = title
         self.publishment = publishment
         self.grade = grade
-        self.number_of_plays = number_of_plays 
+        self.number_of_plays = number_of_plays
 
     def play(self):
         self.number_of_plays += 1
@@ -35,38 +35,32 @@ class Series(Movie):
         return f'Series(title={self.title}, publishment={self.publishment}, grade={self.grade}, number_of_plays={self.number_of_plays}, episode={self.episode:02d}, season={self.season:02d})'
 
 
-library_list = []
-
 def get_movies():
     list_movies= []
     for i in library_list:
-        if not isinstance(i, Series):
+        if type(i) == Movie:
             list_movies.append(i.title)
-    sort_list_movies = sorted(list_movies)
+    sort_list_movies = sorted(list_movies) 
     return sort_list_movies
 
 def get_series():
     list_series= []
     for i in library_list:
-        if isinstance(i, Series):
+        if type(i) == Series:
             list_series.append(i.title)
     sort_list_series = sorted(list_series)        
     return sort_list_series
 
-def search():
-    choice = input('Podaj tytuł: ')
-    choice1 = choice.upper()
-    for i in library_list:
-        if choice1 == i.title.upper():
-            if not isinstance(i, Series):
-                print(f'{i.title} {i.publishment}')
-            else:
-                print(f"{i.title} S{i.season:02d}E{i.episode:02d}")
-
+def search(list, title):
+    for i in list:
+        if title == i.title:
+            found_movie = title
+    return found_movie
+      
 def generate_views():
     random_movie = random.choice(library_list)
     random_number = random.randrange(1, 100)
-    random_movie.number_of_plays = random_movie.number_of_plays + random_number
+    random_movie.number_of_plays += random_number
 
 def generate_views_times():
     for times in range(10):
@@ -79,10 +73,11 @@ def top_titles(value):
 
 
 if __name__ == '__main__':
+    library_list = []
     today = date.today()
     today_format = today.strftime("%d.%m.%Y")
 
-    movie = Movie(title='Nasz palneta', publishment=2019, grade='Document', number_of_plays=20)
+    movie = Movie(title='Nasza planeta', publishment=2019, grade='Document', number_of_plays=20)
     movie2 = Movie(title='BoJack Horseman', publishment=2014, grade='Drama', number_of_plays=10)
     movie3 = Movie(title='Stranger Things', publishment=2016, grade='Sci-Fi', number_of_plays=15)
     series = Series(title='Dom z papieru', publishment=2017, grade='Thriller', number_of_plays=100, episode=2, season=3)
@@ -95,17 +90,9 @@ if __name__ == '__main__':
     library_list.append(series)
     library_list.append(series2)
     library_list.append(series3)
-    
-    print('Biblioteka filmów:')
-    print()
 
-    for i in library_list:
-        if not isinstance(i, Series):
-            print(f'{i.title} {i.publishment}')
-        else:
-            print(f'{i.title} S{i.season:02d}E{i.episode:02d}')
+    print('Biblioteka filmów')
 
-    print()
     generate_views()
     print()
 
@@ -113,8 +100,4 @@ if __name__ == '__main__':
     print()
 
 
-    #search()
-    #generate_views_times()
-    #print(get_movies())
-    #print(get_series())
 
